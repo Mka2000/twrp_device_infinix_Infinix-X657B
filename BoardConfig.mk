8857be2765bd4b5ab48922164e43cecf1c0c1964
Mka2000 <86131760+Mka2000@users.noreply.github.com>
2023-12-26 09:37:31 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X657B
+DEVICE_PATH := device/infinix/X657B
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
