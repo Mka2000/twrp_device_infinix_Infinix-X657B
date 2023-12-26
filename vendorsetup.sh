392b0ba713dce48c21b32d030339ba1c6801900b
Mka2000 <86131760+Mka2000@users.noreply.github.com>
2023-12-26 18:05:24 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,8 +3,5 @@
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
-#
 
-add_lunch_combo omni_Infinix-X657B-user
-add_lunch_combo omni_Infinix-X657B-userdebug
 add_lunch_combo omni_Infinix-X657B-eng
